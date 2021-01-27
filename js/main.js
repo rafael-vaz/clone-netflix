5807734f7d6e64ba14660d4ad73181ffcf7f30be
Rafael Vaz <ferreira.vaz@live.com>
2021-01-27 18:53:37 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:2
+        },
+
+        400:{
+            items:3
+        },
+
+        600:{
+            items:5
+        },
+
+        800:{
+
+            items:5
+        },
+
+        1000:{
+            items:7
+        },
+
+        1300:{
+            items:8
+        }
+    }
+})
